1a1a3325a443736f4ed2ed3516d76118ff56a33c
Диёр <diertojiev@yandex.ru>
2023-01-02 15:15:29 +0300

    Удаление гиперссылки 'Новости'

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,14 +7,15 @@ function scrollTo(element) {
 }
 
 window.onload=function(){
+
     // Плавная прокрутка принажатии на кнопку "Читать далее"
     let read_more_button = document.querySelector(".read_more_button");
     let about_us_1 = document.querySelector("#about_us");
 
     read_more_button.addEventListener('click', () => {
         scrollTo(about_us_1);
-        console.log('Все работает')
-    })
+        console.log('Все работает');
+    });
 
     // Плавная прокрутка при нажатии на гиперссылку "Главная"
     let nav_hyperlink_a_1 = document.querySelector("#nav_hyperlink_a_1");
@@ -22,17 +23,16 @@ window.onload=function(){
 
     nav_hyperlink_a_1.addEventListener('click', () => {
         scrollTo(main_1);
-        console.log('Все работает')
-    })
+        console.log('Все работает');
+    });
 
     // Плавная прокрутка при нажатии на гиперссылку "О нас"
-
     let nav_hyperlink_a_2 = document.querySelector("#nav_hyperlink_a_2");
     let about_us_2 = document.querySelector('#about_us');
 
     nav_hyperlink_a_2.addEventListener('click', () => {
         scrollTo(about_us_2);
-        console.log('Все работает')
-    })
+        console.log('Все работает');
+    });
     
 }
