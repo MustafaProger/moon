7eb1558ec661e085ff35a7f0d707dc617c643bb8
DIOR <diertojiev@yandex.ru>
2024-02-12 11:21:44 +0300

    Исправления ошибки navbar
    Была такая проблема. При нажатии на li в navbar все четко работало, но если я нажимал на что-либо вы ходящее из navbar li, но он оставался активным. Я добавил пару строчек кода, которые говорят, что при нажатии на document, а в данном случаи на что-угодно на сайта, удаляй класс active у li и скрывай меню, а также строчку кода stopPropagation(), которая предотвращаеет всплытие другого события

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,23 @@
-$( document ).ready(function() {
-
-    // Работа скрытого меню в navbar
-    $('.list__item_menu-hide').on('click', function() {
+$(document).ready(function() {
+    // Работа скрытого меню 
+    $('.list__item_menu-hide').on('click', function(e) {
         $(".list__item_menu-hide").not(this).removeClass('active');
-        $(this).toggleClass('active');
-    })
+        $(this).addClass('active');
+        e.stopPropagation(); // Предотвращаем всплытие события
+    });
 
+    //Работа подскрытого меню в скрытом меню
+    $('.sub-menu__list_contact li').on('click', function(e) {
+        $(this).toggleClass('active');
+        e.stopPropagation(); // Предотвращаем всплытие события, чтобы избежать воздействия на родительские элементы
+    });
+
+    // Закрыть все активные меню при клике вне их области
+    $(document).on('click', function() {
+        $('.list__item_menu-hide').removeClass('active');
+    });
+    
+    // slick-слайдер
     $('.game__slick-galeri ').slick({
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/arrowLeft.svg" alt="prevArrow"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/arrowRight.svg" alt="prevArrow"></button>',
