05135db2e09d434a0831e92e05ac7214ddbb5b90
DIOR <diertojiev@yandex.ru>
2024-02-12 10:41:37 +0300

    Готова функция со slick-слайдер

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,16 @@
 $( document ).ready(function() {
+
+    // Работа скрытого меню в navbar
     $('.list__item_menu-hide').on('click', function() {
         $(".list__item_menu-hide").not(this).removeClass('active');
         $(this).toggleClass('active');
     })
+
+    $('.game__slick-galeri ').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/arrowLeft.svg" alt="prevArrow"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/arrowRight.svg" alt="prevArrow"></button>',
+        speed: 500,
+        dots: true
+    });
 });
 
